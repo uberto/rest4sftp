8301b7cca849908bfdffc371effb639ce964c4d2
Uberto Barbini <uberto.barbino@springernature.com>
2019-02-07 13:45:39 +0000

    SshJ implementation

--- src/main/kotlin/org/rest4sftp/ftp/SshJSftpClient.kt
+++ src/main/kotlin/org/rest4sftp/ftp/SshJSftpClient.kt
@@ -4,17 +4,20 @@ import net.schmizz.sshj.DefaultConfig
 import net.schmizz.sshj.SSHClient
 import net.schmizz.sshj.sftp.RemoteResourceInfo
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
+import net.schmizz.sshj.xfer.InMemoryDestFile
+import net.schmizz.sshj.xfer.InMemorySourceFile
 import org.apache.commons.net.ftp.FTPFile
 import org.rest4sftp.model.RemoteHost
 import org.rest4sftp.model.SimpleRemoteClient
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
-
+import java.io.OutputStream
+import java.nio.file.Paths
 
 
 class SshJSftpClient(val remoteHost: RemoteHost): SimpleRemoteClient {
 
     val sshClient = SSHClient(DefaultConfig())
-
 
     fun toFtpFile(rrinfo: RemoteResourceInfo): FTPFile =
         FTPFile().apply {
@@ -23,40 +26,51 @@ class SshJSftpClient(val remoteHost: RemoteHost): SimpleRemoteClient {
         }
 
     override fun listFiles(directoryName: String): List<FTPFile> =
-        sshClient.newSFTPClient().use {
+        runCatching {
+            sshClient.newSFTPClient().use {
             sftpClient -> sftpClient.ls(directoryName).map ( ::toFtpFile )
-        }
+        }}.getOrDefault(emptyList())
 
-    override fun createFolder(directoryName: String): Boolean {
-        sshClient.newSFTPClient().use {
+    override fun createFolder(directoryName: String): Boolean =
+        runWithNoExceptions {
+            sshClient.newSFTPClient().use {
             sftpClient -> sftpClient.mkdir(directoryName)
-        }
-        return true
-    }
+            } }
 
-    override fun deleteFolder(directoryName: String): Boolean {
-        sshClient.newSFTPClient().use {
-            sftpClient -> sftpClient.rmdir(directoryName)
-        }
-        return true
-    }
+    override fun deleteFolder(directoryName: String): Boolean =
+        runWithNoExceptions {
+            sshClient.newSFTPClient().use { sftpClient ->
+                sftpClient.rmdir(directoryName)
+            } }
 
-    override fun retrieveFile(directoryName: String, fileName: String): ByteArray {
-//        sFtpServer.get()rmdir(directoryName)
-        TODO()
+    override fun retrieveFile(directoryName: String, fileName: String): ByteArray =
+        runCatching {
+            InMemoryOutputFile().also {
+                sshClient.newSFTPClient().use { sftpClient ->
+                    sftpClient.get(directoryName slash fileName, it)
+                }
+            }.outputStream.toByteArray()
+        }.getOrDefault(ByteArray(0))
 
-    }
+    override fun uploadFile(directoryName: String, fileName: String, upload: InputStream): Boolean =
+        runWithNoExceptions {
+            val sourceFile = InMemoryInputFile(upload)
+            sshClient.newSFTPClient().use {
+                sftpClient -> sftpClient.put(sourceFile, directoryName slash fileName)
+                }
+            }
 
-    override fun uploadFile(directoryName: String, fileName: String, upload: InputStream): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun deleteFile(directoryName: String, fileName: String): Boolean {
-        sshClient.newSFTPClient().use {
-            sftpClient -> sftpClient.rm(directoryName + "/" + fileName)
-        }
-        return true
-    }
+    override fun deleteFile(directoryName: String, fileName: String): Boolean =
+         runWithNoExceptions {
+             sshClient.newSFTPClient().use { sftpClient ->
+                 sftpClient.rm(directoryName slash fileName)
+             }
+         }
+
+    private fun runWithNoExceptions(block: () -> Unit): Boolean =
+        runCatching(block).isSuccess
+
 
     override fun connect(): SimpleRemoteClient {
         sshClient.addHostKeyVerifier(PromiscuousVerifier())
@@ -70,3 +84,21 @@ class SshJSftpClient(val remoteHost: RemoteHost): SimpleRemoteClient {
     }
 
 }
+
+private infix fun String.slash(fileName: String): String = Paths.get(this, fileName).toString()
+
+
+class InMemoryOutputFile(): InMemoryDestFile(){
+    val outputStream = ByteArrayOutputStream()
+
+    override fun getOutputStream(): OutputStream = outputStream
+}
+
+class InMemoryInputFile(val uploadStream: InputStream) : InMemorySourceFile(){
+    override fun getLength(): Long = 0
+
+    override fun getName(): String = "MemoryFile"
+
+    override fun getInputStream(): InputStream = uploadStream
+}
+
