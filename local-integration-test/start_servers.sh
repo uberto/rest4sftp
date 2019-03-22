45d39898154d6722d2be15e64bb2cf0e9dc2ec3d
anuraopadev-springer <anuraopadev@springernature.com>
2019-03-22 09:56:34 +0000

    [UB] Updated permissions and git ignore

--- local-integration-test/start_servers.sh
+++ local-integration-test/start_servers.sh
@@ -3,6 +3,6 @@
 readonly BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${BASE_DIR}"
-chmod -R 777 ./ftp-content-root
+chmod -R 755 ./ftp-content-root
 chmod 400 ./ssh-keys/*
 docker-compose up -d
